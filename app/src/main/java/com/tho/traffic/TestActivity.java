f20b7b5bbcb3e4781313fb3217289d88bf14b471
Surasee <thorsu@hotmail.com>
2015-08-19 15:12:34 +0700

    Day 3 V.1.2

--- app/src/main/java/com/tho/traffic/TestActivity.java
+++ app/src/main/java/com/tho/traffic/TestActivity.java
@@ -1,5 +1,6 @@
 package com.tho.traffic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -16,20 +17,103 @@ public class TestActivity extends AppCompatActivity {
     private RadioGroup choiceRadioGroup;
     private RadioButton choice1RadioButton, choice2RadioButton,
             choice3RadioButton, choice4RadioButton;
+
     private Button button;
+
+    private String[] questionStrings;
+    private int[] imageInts;
+    private int radioAnInt;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        questionTextView = (TextView) findViewById(R.id.txtQuestion);
-        TrafficImageView = (ImageView) findViewById(R.id.imvTraffic);
-        choiceRadioGroup = (RadioGroup) findViewById(R.id.ragChoice);
-        button = (Button) findViewById(R.id.button2);
+
+
+        //เชื่อมกับ xml
+        BindWidget();
+
+
+
+        //radio Controller
+        radioController();
+
+
+
+    }//oncreate
+
+    private void radioController() {
+        choiceRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                switch (i){
+                    case R.id.radioButton:
+                        radioAnInt = 1;
+                        break;
+                    case R.id.radioButton2:
+                        radioAnInt = 2;
+                        break;
+                    case R.id.radioButton3:
+                        radioAnInt = 3;
+                        break;
+                    case R.id.radioButton4:
+                        radioAnInt = 4;
+                        break;
+                    default:
+                        radioAnInt = 0;
+                        break;
+                }
+
+            }
+        });
 
 
     }
+
+    private void BindWidget() {
+        questionTextView = (TextView) findViewById(R.id.txtQuestion);
+        TrafficImageView = (ImageView) findViewById(R.id.imvTraffic);
+        choiceRadioGroup = (RadioGroup) findViewById(R.id.ragChoice);
+        choice1RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        choice2RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        choice3RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        choice4RadioButton = (RadioButton) findViewById(R.id.radioButton4);
+
+    }
+
+    @Override
+    protected void onStart() {
+        //setup
+
+        questionStrings = getResources().getStringArray(R.array.question);
+
+        imageInts = new int[10];
+        imageInts[0] = R.drawable.traffic_01;
+        imageInts[1] = R.drawable.traffic_02;
+        imageInts[2] = R.drawable.traffic_03;
+        imageInts[3] = R.drawable.traffic_04;
+        imageInts[4] = R.drawable.traffic_05;
+        imageInts[5] = R.drawable.traffic_06;
+        imageInts[6] = R.drawable.traffic_07;
+        imageInts[7] = R.drawable.traffic_08;
+        imageInts[8] = R.drawable.traffic_09;
+        imageInts[9] = R.drawable.traffic_10;
+
+        String[] strChoice = getResources().getStringArray(R.array.times1);
+
+        //Just Start
+        questionTextView.setText(questionStrings[0]);
+        TrafficImageView.setImageResource(imageInts[0]);
+        choice1RadioButton.setText(strChoice[0]);
+        choice2RadioButton.setText(strChoice[1]);
+        choice3RadioButton.setText(strChoice[2]);
+        choice4RadioButton.setText(strChoice[3]);
+        super.onStart();
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
